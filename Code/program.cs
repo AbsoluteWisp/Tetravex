4eaa625c3d62714a635cae5bf6a4fdb3dda33e85
Ghostling225 <wojcikadam742@gmail.com>
2021-11-23 22:03:45 +0100

    Create board generator
    Create a random board generator and prettyprint it to the console

--- Code/program.cs
+++ Code/program.cs
@@ -1,5 +1,58 @@
 ﻿public static class Program {
 	public static void Main(string[] args) {
-		Console.Write("Hello, world!\n");
+		const int boardX = 7;
+		const int boardY = 7;
+
+		// Instantiate the UI class
+		UI ui = new UI();
+
+		// Generate a solved board
+		model.MapGenerator tilegen = new model.MapGenerator();
+		model.Tile[,] boardMap = tilegen.GenerateMap(boardX, boardY);
+
+		// Prettyprint the board
+		ui.PrettyPrintBoard(boardMap, boardX, boardY);
+	}
+}
+
+public class UI {
+	public void PrettyPrintBoard(model.Tile[,] boardMap, int boardX, int boardY) {
+		// Repeat the top separation line for boardX times and add the missing end
+		for (int i = 0; i < boardX; i++) {
+			Console.Write("+-----");
+		}
+		Console.Write("+\n");
+		
+		for (int y = 0; y < boardY; y++) {
+			// Start of row
+			Console.Write("|");
+			// First line: top value of each tile in the row
+			for (int x = 0; x < boardX; x++) {
+				Console.Write("\\ " + boardMap[x, y].U + " /|");
+			}
+			Console.Write("\n");
+			
+			// Start of row
+			Console.Write("|");
+			// Second line: left and right values of each tile in the row
+			for (int x = 0; x < boardX; x++) {
+				Console.Write(boardMap[x, y].L + " X " + boardMap[x, y].R + "|");
+			}
+			Console.Write("\n");
+
+			// Start of row
+			Console.Write("|");
+			// Third line: down value of each tile in the row
+			for (int x = 0; x < boardX; x++) {
+				Console.Write("/ " + boardMap[x, y].D + " \\|");
+			}
+			Console.Write("\n");
+			
+			// Repeat the bottom separation line for boardX times and add the missing end
+			for (int i = 0; i < boardX; i++) {
+				Console.Write("+-----");
+			}
+			Console.Write("+\n");
+		}
 	}
 }
